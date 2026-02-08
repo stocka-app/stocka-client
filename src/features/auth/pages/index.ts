8999b1cc3defa6450937db794247120a5ffbb062
2314-RAustin <0315108515@miutt.edu.mx>
2026-02-07 20:44:32 -0800

    feat(auth): export new pages from barrel
    - Export VerifyEmailPage and OAuthCallbackPage

--- src/features/auth/pages/index.ts
+++ src/features/auth/pages/index.ts
@@ -1,2 +1,4 @@
 export { LoginPage } from './LoginPage'
 export { RegisterPage } from './RegisterPage'
+export { VerifyEmailPage } from './VerifyEmailPage'
+export { OAuthCallbackPage } from './OAuthCallbackPage'
